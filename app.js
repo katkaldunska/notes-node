ca7a4ec3b96333bca4fb14fe9888af333d9c7126
Katarzyna Kaldunska <k.kaduska@gmail.com>
2018-06-24 20:02:19 +0200

    add crud

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 var command = argv._[0];
-console.log('Command: ', command);
-// console.log('Process', process.argv);
-console.log('Yargs', argv);
 
 if (command === 'add') {
-  // console.log('Adding new note');
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('--Note created successfully--');
+    notes.logNote(note);
+  } else {
+    console.log(`--Error occured-- \n Note with title ${argv.title} already exists`);
+  }
 } else if (command === 'list') {
-  // console.log('Listing all notes');
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`--Printing ${allNotes.length} note(s)--`);
+  allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read') {
-  // console.log('Reading note');
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('--Note found--');
+    notes.logNote(note);
+  } else {
+    console.log('--Note not found--');
+  }
 } else if (command === 'remove') {
-  // console.log('Removing note');
-  notes.removeNote(argv.title);
-}  else {
-  console.log('Command not recognized');
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? '--Note was removed--' : '--Note not found--';
+  console.log(message);
+} else {
+  console.log('--Command not recognized--');
 }
